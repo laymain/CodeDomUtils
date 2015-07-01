4229bf27f817466dd593508a1c5adf29b32bc3c8
Laymain <laymain@gmail.com>
2015-07-02 00:01:43 +0200

    Missing classes FluentCodeClass and FluentCodeInterface added (by disassembling FluentCodeDom.dll)

---
+++ FluentCodeDom/FluentCodeClass.cs
@@ -0,0 +1,36 @@
+﻿namespace FluentCodeDom
+{
+    using System;
+    using System.CodeDom;
+
+    public class FluentCodeClass : FluentCodeType<FluentCodeClass>
+    {
+        public FluentCodeClass() : this(new CodeTypeDeclaration())
+        {
+        }
+
+        public FluentCodeClass(CodeTypeDeclaration type) : this(type, null)
+        {
+        }
+
+        public FluentCodeClass(CodeTypeDeclaration type, FluentCodeNamespace ns) : base(type, ns)
+        {
+        }
+
+        public FluentCodeClass(MemberAttributes attributes, string className) : this(new CodeTypeDeclaration())
+        {
+            base.Name(className);
+            base.Attributes(attributes);
+            base.IsClass();
+        }
+
+        public FluentCodeNamespace EndClass
+        {
+            get
+            {
+                return this.EndInternal;
+            }
+        }
+    }
+}
+
---
+++ FluentCodeDom/FluentCodeInterface.cs
@@ -0,0 +1,36 @@
+﻿namespace FluentCodeDom
+{
+    using System;
+    using System.CodeDom;
+
+    public class FluentCodeInterface : FluentCodeType<FluentCodeInterface>
+    {
+        public FluentCodeInterface() : this(new CodeTypeDeclaration())
+        {
+        }
+
+        public FluentCodeInterface(CodeTypeDeclaration type) : this(type, null)
+        {
+        }
+
+        public FluentCodeInterface(CodeTypeDeclaration type, FluentCodeNamespace ns) : base(type, ns)
+        {
+        }
+
+        public FluentCodeInterface(MemberAttributes attributes, string interfaceName) : this(new CodeTypeDeclaration())
+        {
+            base.Name(interfaceName);
+            base.Attributes(attributes);
+            base.IsInterface();
+        }
+
+        public FluentCodeNamespace EndInterface
+        {
+            get
+            {
+                return this.EndInternal;
+            }
+        }
+    }
+}
+
